c8dfa26b2cb502b7ca3134af7a83e7265628bb35
Alexander-Tatymov <ya.ya.wolf567@gmail.com>
2024-11-18 12:36:30 +0300

    Add files via upload

---
+++ Task_part3_Tatymov_Alexander/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class CharacterCounter
+{
+    static void Main()
+    {
+        Console.Write("Введите предложение: ");
+        string input = Console.ReadLine();
+
+        Console.Write("Введите символ для поиска: ");
+        char characterToCount;
+
+        while (true)
+        {
+            string inputChar = Console.ReadLine();
+            if (inputChar.Length == 1 && char.TryParse(inputChar, out characterToCount))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Пожалуйста, введите один символ.");
+            }
+        }
+
+        int count = 0;
+        foreach (char c in input)
+        {
+            if (c == characterToCount)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Символ '{characterToCount}' встречается в предложении {count} раз(а).");
+    }
+}
